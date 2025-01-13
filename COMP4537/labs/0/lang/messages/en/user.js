d559c660a96ccc2411bdec9c7919de63dfcf1afb
PhoenixAlpha204 <quincywong00@gmail.com>
2025-01-12 23:34:12 -0800

    Add lab 0 files

---
+++ COMP4537/labs/0/lang/messages/en/user.js
@@ -0,0 +1,7 @@
+{
+  "instruction": "How many buttons to create?",
+  "start": "Go!",
+  "win": "Excellent memory!",
+  "lose": "Wrong order!",
+  "inputError": "Input must be a number between 3 and 7 inclusive."
+}
