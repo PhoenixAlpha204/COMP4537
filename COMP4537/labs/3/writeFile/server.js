09630f9a3f21f807de5540095c0b216650a26798
PhoenixAlpha204 <quincywong00@gmail.com>
2025-02-02 22:57:06 -0800

    try s3

--- COMP4537/labs/3/writeFile/server.js
+++ COMP4537/labs/3/writeFile/server.js
@@ -1,18 +1,51 @@
+// const http = require("http");
+// const url = require("url");
+// const fs = require("fs");
+
+// http
+//   .createServer((req, res) => {
+//     const q = url.parse(req.url, true);
+//     const data = `${q.query["text"]}\n`;
+//     fs.appendFile("../file.txt", data, { flag: "a" }, (err) => {
+//       if (err) {
+//         res.writeHead(500, { "Content-Type": "text/html" });
+//         return res.end(`Error appending to file: ${err}`);
+//       }
+//       res.writeHead(200, { "Content-Type": "text/html" });
+//       return res.end("Data appended successfully!");
+//     });
+//   })
+//   .listen(process.env.PORT || 8888);
+
 const http = require("http");
 const url = require("url");
-const fs = require("fs");
+const AWS = require("aws-sdk");
+
+// Configure AWS SDK
+AWS.config.update({ region: "us-east-2" }); // e.g., 'us-east-1'
+const s3 = new AWS.S3();
+
+const bucketName = "phoenixalpha-comp4537"; // Replace with your S3 bucket name
 
 http
   .createServer((req, res) => {
     const q = url.parse(req.url, true);
     const data = `${q.query["text"]}\n`;
-    fs.appendFile("../file.txt", data, { flag: "a" }, (err) => {
+    const params = {
+      Bucket: bucketName,
+      Key: "file.txt", // Unique file name
+      Body: data,
+      ContentType: "text/plain",
+    };
+
+    // Upload data to S3
+    s3.upload(params, (err, data) => {
       if (err) {
-        res.writeHead(500, { 'Content-Type': 'text/html' });
+        res.writeHead(500, { "Content-Type": "text/html" });
         return res.end(`Error appending to file: ${err}`);
       }
-      res.writeHead(200, { 'Content-Type': 'text/html' });
-      return res.end('Data appended successfully!');
+      res.writeHead(200, { "Content-Type": "text/html" });
+      return res.end("Data appended successfully!");
     });
   })
-  .listen(process.env.PORT || 8888);
+  .listen(process.env.port || 8888);
