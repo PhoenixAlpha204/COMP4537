24c77315d5492545adf5d5cc011e64389dff1c96
PhoenixAlpha204 <quincywong00@gmail.com>
2025-02-02 23:40:49 -0800

    make getDate oop

--- COMP4537/labs/3/getDate/modules/utils.js
+++ COMP4537/labs/3/getDate/modules/utils.js
@@ -1,3 +1,8 @@
-exports.datetime = () => {
-  return new Date().toString();
-};
+class DateTime {
+  constructor() {}
+  datetime() {
+    return new Date().toString();
+  }
+}
+const dateTimeInstance = new DateTime();
+module.exports = dateTimeInstance;
--- COMP4537/labs/3/getDate/server.js
+++ COMP4537/labs/3/getDate/server.js
@@ -1,14 +1,30 @@
-const utils = require("./modules/utils");
 const http = require("http");
 const url = require("url");
+const utils = require("./modules/utils");
 const strings = require("./lang/en/en.json");
 
-http
-  .createServer((req, res) => {
-    let q = url.parse(req.url, true);
+class Server {
+  constructor(port) {
+    this.port = port;
+  }
+
+  handleRequest(req, res) {
+    const q = url.parse(req.url, true);
     res.writeHead(200, { "Content-Type": "text/html" });
     res.end(`<p style="color:blue;">
-      ${strings.greeting.replace("%1", q.query["name"])}${utils.datetime()}
+      ${strings.greeting.replace("%1", q.query["name"])} ${utils.datetime()}
       </p>`);
-  })
-  .listen(process.env.PORT || 8888);
+  }
+
+  start() {
+    const server = http.createServer((req, res) =>
+      this.handleRequest(req, res)
+    );
+    server.listen(this.port, () => {
+      console.log(`Server is running on port ${this.port}`);
+    });
+  }
+}
+
+const server = new Server(process.env.PORT || 8888);
+server.start();
